86fc7658107ca90745e9ce4cd471431b886acb86
MatamorosF3 <matamorosf3@gmail.com>
2013-09-19 23:50:01 -0600

    Aqui va la clase producto!! Agujas papa!!! ahora solo descomenta el ingresar producto de la clase Cliente

---
+++ finalSupermercado04/src/finalsupermercado04/Producto.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalsupermercado04;
+
+/**
+ *
+ * @author Francisco Matamoros
+ */
+public class Producto {
+        private String codigo; // codigo del producto
+    private int precio; // precio del producto
+    private int cantidad; // cantidad del producto
+
+    public Producto(String codigo, int precio, int cantidad) { // constructor con parametros del producto
+        this.codigo = codigo;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getCodigo() { // metodo para obtener el codigo
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) { // metodo set para cambiar el codigo
+        this.codigo = codigo;
+    }
+
+    public int getPrecio() { // metodo para obtener el precio
+        return precio;
+    }
+
+    public void setPrecio(int precio) { // metodo para cambiar el precio
+        this.precio = precio;
+    }
+
+    public int getCantidad() { // metodo para obtener la cantidad 
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) { // metodo para cambiar la cantidad
+        this.cantidad = cantidad;
+    }
+
+}
