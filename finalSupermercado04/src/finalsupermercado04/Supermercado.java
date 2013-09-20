c3dea4cf23f4ca0bd273e85f66c77efa89857731
swammj <swammj@gmail.com>
2013-09-20 01:02:58 -0600

    MIra que hice este metodo pa poner el cliente en una caja, la caja1 es la express

--- finalSupermercado04/src/finalsupermercado04/Supermercado.java
+++ finalSupermercado04/src/finalsupermercado04/Supermercado.java
@@ -28,7 +28,67 @@ public class Supermercado {
         this.totalRegalos = 0;
         this.totalVentas = 0;
     }
+    
+     public String buscarCaja(Cliente cliente) { // metodo utilizado para asignar una cajera al cliente
 
+        //Cuento la cantidad de productos en el carro
+        int cantProd = cliente.getCanasta().size;
+
+        //Cantidad Clientes por caja
+        int ccc1 = this.caja1.size;
+        int ccc2 = this.caja2.size;
+        int ccc3 = this.caja3.size;
+        int ccc4 = this.caja4.size;
+
+        int caja = 0;
+        if (cantProd <= 10) {
+            /*
+             * Cuando la cantidad de producto no es mucha
+             */
+            //Puedo ingresar a caja express
+            caja = 1;
+            if (ccc2 == 0) {
+                caja = 2;
+            }
+            if (ccc3 == 0) {
+                caja = 3;
+            }
+            if (ccc4 == 0) {
+                caja = 4;
+            }
+
+        } else {
+            //No puedo ingresar a caja express
+            caja = 2;
+            if (ccc3 <= ccc4 && ccc3 <= ccc2) {
+                caja = 3;
+            }
+            if (ccc4 <= ccc2 && ccc4 <= ccc3) {
+                caja = 4;
+            }
+
+        }
+        
+        /*
+         * En esta parte es donde agregamos clientes
+         * a las distintas colas creadas anteriormente
+         */
+
+        if (caja == 1) {
+            this.caja1.queue(cliente);
+        }
+        if (caja == 2) {
+            this.caja2.queue(cliente);
+        }
+        if (caja == 3) {
+            this.caja3.queue(cliente);
+        }
+        if (caja == 4) {
+            this.caja4.queue(cliente);
+        }
+
+        return "El cliente ingresó a la caja " + caja; // mostramos la caja en la que el cliente fue asignado
+    }
 
     public String imprimirReportes() {
         String s = "";
